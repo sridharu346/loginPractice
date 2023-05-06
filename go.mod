9491566c2cda147c8fdbadf26523847127d39856
sridharu346 <sridharu346@gmail.com>
2023-05-06 16:31:36 -0400

    included the mod files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sridharu346/loginPractice
+
+go 1.20
+
+require github.com/go-sql-driver/mysql v1.7.1
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	DBconfig "github.com/sridharu346/loginPractice/dbconfig"
+)
+
 func main() {
 
+	DB, err := DBconfig.DBConection()
+	if err != nil {
+		fmt.Print("Error Occured")
+	}
+	defer DB.Close()
+	fmt.Print("DB connected Successfully")
 }
